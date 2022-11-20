e01de4408084887198af215e7b064373148b22bd
Racquel Munoz <racquelg85@hotmail.com>
2022-11-20 15:07:12 -0600

    4th stagging

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development",
@@ -11,13 +11,13 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-      title: "Webpack Plugin",
-    }),
     new InjectManifest({
       swSrc: "./src/sw.js",
       swDest: "service-worker.js",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "Webpack Plugin",
     }),
   ],
   module: {
